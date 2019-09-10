c8f9c81f6f2cb9c355a56243684d132b4c551a46
Niklas Mollenhauer <nikeee@outlook.com>
2019-09-10 17:48:19 +0200

    Add type hints

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,12 +1,25 @@
+// @ts-check
 
+/**
+ * @param {number} min
+ * @param {number} max
+ */
 export function randomNumber(min, max) {
   return min + Math.random() * (max - min);
 }
 
+/**
+ * @param {number} min
+ * @param {number} max
+ */
 export function randomInt(min, max) {
-  return min + Math.floor(Math.random() * Math.floor(max - min));
+  const span = (max - min) | 0;
+  return min + ((Math.random() * span) | 0);
 }
 
+/**
+ * @param {unknown[]} options
+ */
 export function randomOption(options) {
   const selected = randomInt(0, options.length);
   return options[selected];
